f0bbc79b4e4f3a85797088e6a50ce99d4d69135a
Alex Merced <alex@alexmerced.dev>
2022-03-25 15:47:55 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-$.ajax("https://api.icndb.com/jokes/random")
-.then((data) => {
+const addSquare = (joke) => {
+    const square = document.createElement("div")
+    square.classList.add("square")
+    square.innerHTML = `<h5>${joke}</h5>`
+    const container = document.querySelector(".joke-container")
+    container.append(square)
+}
+
+const getJokeMakeSquare = () => {
+    $.ajax("https://api.icndb.com/jokes/random")
+    .then((data) => {
     console.log(data)
     console.log(data.value.joke)
+    addSquare(data.value.joke)
 })
+}
+
+getJokeMakeSquare()
+
+const button = document.querySelector("button")
+button.addEventListener("click", getJokeMakeSquare)
+
+
